3712a507a2d3e0b83ff8a9e9a3d2eb93948767a4
AMK <AMK@DESKTOP-QL5CJKJ>
2024-11-09 12:09:20 -0500

    Add project files.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Catee_Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CategoryOrder { get; set; }
+    }
+}
